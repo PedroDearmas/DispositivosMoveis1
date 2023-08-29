4768ba210fbc383310a572ea1a4d673b51768895
Pedro <pedrodearmas1234@gmail.com>
2023-08-28 21:38:11 -0300

    Finalizando aula 2

--- somar/App.js
+++ somar/App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  const [valor1, setValor1] = useState("30");
-  const [valor2, setValor2] = useState("30");
+  const [valor1, setValor1] = useState("");
+  const [valor2, setValor2] = useState("");
   let resultado = 0;
 
   const soma = () => {
-    resultado = valor1 + valor2;
-    alert("O resultado é;" + resultado);
+    resultado = parseFloat(valor1) + parseFloat(valor2);
+    alert("O resultado é " + resultado);
+  };
+  const subtrair = () => {
+    resultado = parseFloat(valor1) - parseFloat(valor2);
+    alert("O resultado é " + resultado);
+  };
+  const dividir = () => {
+    resultado = parseFloat(valor1) / parseFloat(valor2);
+    alert("O resultado é " + resultado);
+  };
+  const multiplicar = () => {
+    resultado = parseFloat(valor1) * parseFloat(valor2);
+    alert("O resultado é " + resultado);
   };
 
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <TextInput
+        style={styles.inputs}
+        placeholder="Digite Valor 1"
+        keyboardType="numeric"
+        onChangeText={(valor1) => setValor1(valor1)}
+        value={valor1}
+      ></TextInput>
+
+      <TextInput
+        style={styles.inputs}
+        placeholder="Digite Valor 2"
+        keyboardType="numeric"
+        onChangeText={(valor2) => setValor2(valor2)}
+        value={valor2}
+      ></TextInput>
+
+      <View style={styles.botao}>
+        <Button title="Somar" onPress={soma}></Button>
+        <Button title="Subtrair" onPress={subtrair}></Button>
+        <Button title="Dividir" onPress={dividir}></Button>
+        <Button title="Multiplicar" onPress={multiplicar}></Button>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -27,4 +61,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  inputs: {
+    marginBottom: "3%",
+  },
+  botao: {},
 });
