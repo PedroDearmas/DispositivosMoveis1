948dd2332e5c3beb39a90c60f1b38bfc5ea6479b
Pedro Araujo Dearmas <115725683+PedroDearmas@users.noreply.github.com>
2023-11-27 20:42:10 -0300

    Aula 27/11

---
+++ EpicSportsStore/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={ } />
+        <Drawer.Screen name="Notifications" component={ } />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
